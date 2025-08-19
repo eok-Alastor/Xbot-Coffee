24d695fe1591ce3a6eed1232d764ea7fe42a630d
eok-Alastor <1093312330@qq.com>
2025-08-19 12:44:12 +0800

    Vercel部署问题第二次修复

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // astro.config.mjs
+
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  // 只保留这一项！
+  integrations: [tailwind()] 
 });
